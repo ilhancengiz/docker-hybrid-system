4351291da30bfb73a9db7a05f0677a0adf0795bf
İlhan Cengiz <23198866+ilhancengiz@users.noreply.github.com>
2018-06-06 16:31:37 +0300

    Update app.py

--- KeyBackground/app.py
+++ KeyBackground/app.py
@@ -5,6 +5,7 @@ import time
 def get_redis():
     if not hasattr(g, 'redis'):
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
+    print ("Connected to redis from python!")
     return g.redis
 
 def connectPostgre():
@@ -19,15 +20,28 @@ def connectPostgre():
             cursor.execute("""SELECT * from keyValues""")
             rows = cursor.fetchall()
             for row in rows:
-                print("Key: {0}, Value: {1}".format(row[0], row[1]))       
+                print("Key: {0}, Value: {1}".format(row[0], row[1]))
+            return rows				
         except:
             print("I can't SELECT from keyValues")
     except:
         print("I am unable to connect to the database.")
 
+def setRedisValuesFromPostgres(redis, dbValues):
+    # first delete all keys from redis
+    for key in redis.scan_iter():
+	    redis.delete(key)
+    print ("All Redis values deleted!")	
+	# set new values from db
+    for row in dbValues:
+        redis.set(row[0], row[1])		
+    print ("Redis values updated from db values!")		
+		
 def init():
     while True:
-        connectPostgre()
+        dbValues = connectPostgre()
+		redis = get_redis()	
+        setRedisValuesFromPostgres(redis, dbValues)		
         time.sleep(30)
         
 if __name__ == "__main__":
