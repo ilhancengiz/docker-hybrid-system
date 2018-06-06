33db87962a32cfdb5e3ab1d7876bb11e303843b5
İlhan Cengiz <23198866+ilhancengiz@users.noreply.github.com>
2018-06-06 17:09:53 +0300

    Update ValuesController.cs

--- KeyPublicWebApi/Controllers/ValuesController.cs
+++ KeyPublicWebApi/Controllers/ValuesController.cs
@@ -11,18 +11,26 @@ namespace KeyPublicWebApi.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-		private readonly IDistributedCache _distributedCache;
-		public ValuesController(IDistributedCache distributedCache)
+	private readonly IDistributedCache _distributedCache;
+	public ValuesController(IDistributedCache distributedCache)
         {
             _distributedCache = distributedCache;
         }
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(string id)
-        {
-            var cachedMessage = _distributedCache.GetString(id);
-            return cachedMessage;
-        }
+        
+        [HttpGet]
+	public IEnumerable<string> Get()
+	{
+	var cachedMessage = _distributedCache.GetString("i0");
+	return new List<string> { cachedMessage };
+	}
 
-    }
+	// GET api/values/5
+	[HttpGet("{id}")]
+	public string Get(string id)
+	{
+	var cachedMessage = _distributedCache.GetString(id);
+	return cachedMessage;
+	}
+
+	}
 }
