594e8d2bddb1bc6495fe1a7f0cfc4a825a230e6c
İlhan Cengiz <23198866+ilhancengiz@users.noreply.github.com>
2018-06-06 15:24:05 +0300

    Add files via upload

---
+++ KeyBackground/app.py
@@ -0,0 +1,35 @@
+from redis import Redis
+import psycopg2
+import time
+
+def get_redis():
+    if not hasattr(g, 'redis'):
+        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+    return g.redis
+
+def connectPostgre():
+    conn_string = "postgres://postgres@db/postgres"
+    # get a connection, if a connect cannot be made an exception will be raised here    
+    try:
+        conn = psycopg2.connect(conn_string)
+        # conn.cursor will return a cursor object, you can use this cursor to perform queries
+        cursor = conn.cursor()
+        print ("Connected!\n")
+        try:
+            cursor.execute("""SELECT * from keyValues""")
+            rows = cursor.fetchall()
+            for row in rows:
+                print("Key : ", row["key"], " Value: ", row["value"])            
+        except:
+            print("I can't SELECT from keyValues")
+    except:
+        print("I am unable to connect to the database.")
+
+def init():
+    while True:
+        connectPostgre()
+        # Sleep for a minute
+        time.sleep(60)
+        
+if __name__ == "__main__":
+    init()
---
+++ KeyManagement/server.js
@@ -0,0 +1,122 @@
+var express = require('express'),
+    async = require('async'),
+    pg = require("pg"),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    app = express(),
+    server = require('http').Server(app);
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port);    
+   CreateTableIfNotExists();
+})
+
+
+function updateKey(key, value) { 
+   pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
+        if (err) throw err;
+        console.log("Connected to db");
+        client.query('UPDATE keyValues SET value = $2 WHERE key = $1', [key, value], function(err, result) {
+            if (err) {
+              console.error("Error performing query: " + err);
+            } else {
+              console.log("key updated!");
+              client.end();
+            }
+          });
+  });
+}
+
+function insertKey(key, value) {
+    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
+        if (err) throw err;
+        console.log("Connected to db");
+        client.query('INSERT INTO keyValues(key,value) VALUES($1,$2)', [key, value], function(err, result) {
+            if (err) {
+              console.error("Error performing query: " + err);
+            } else {
+              console.log("key inserted!");
+              client.end();
+            }
+          });
+  });
+}
+
+function getKeys(callback) {
+    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
+        if (err) throw err;
+        console.log("Connected to db");
+        client.query('SELECT key, value FROM keyValues', [], function(err, result) {
+            if (err) {
+              console.error("Error performing query: " + err);
+            } else {
+              console.log(result);  
+              var dictinary = getKeysDictionary(result);
+              callback(JSON.stringify(dictinary));
+            }
+         });
+   });
+}
+
+function getKeysDictionary(result) {
+  var keyPairs = {};
+  result.rows.forEach(function (row) {
+    keyPairs[row.key] = row.value;
+  }); 
+  return keyPairs;
+}
+
+function CreateTableIfNotExists()
+{
+    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
+    if (err) throw err;
+    console.log("Connected to db");
+    client.query('CREATE TABLE IF NOT EXISTS keyValues(key VARCHAR(40) PRIMARY KEY, value VARCHAR(40) not null)', (err, res) => {
+          if (err) throw err;
+          console.log(res);
+          client.end();
+        });    
+    client.query('DELETE FROM keyValues', (err, res) => {
+          if (err) throw err;
+          console.log(res);
+          client.end();
+        });
+  });
+}
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
+//app.use(express.static(__dirname + '/views'));
+
+app.get('/', function (req, res) {
+  getKeys(function(ret) {
+      res.send('Greetings! Here keys -> : ' + ret);
+  });
+});
+
+
+app.post('/newKey', function (req, res) {
+   console.log("Key -> " + req.body.Key);
+   console.log("Value -> " + req.body.Value);
+   insertKey(req.body.Key, req.body.Value);
+   res.send('Key Inserted!');
+})
+
+app.post('/updateKey', function (req, res) {
+   console.log("Key -> " + req.body.Key);
+   console.log("Value -> " + req.body.Value);
+   updateKey(req.body.Key, req.body.Value);
+   res.send('Key Updated!');
+})
